dffdf3c97cd34400335b4d2a2761138de2bfbe16
SimonBD1 <simo20k3@stud.kea.dk>
2023-02-09 17:43:56 +0100

    hele molevitten på en gang

---
+++ src/main/java/com/example/portfoliowebside/PortfoliowebsideApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfoliowebside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliowebsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoliowebsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfoliowebside/PortfoliowebsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfoliowebside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliowebsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
